2d91b64d179d6b6819568f331a55e2044006b6b1
名锐 高 <AmayerGogh@outlook.com>
2017-10-24 22:19:04 +0800

    no message

--- _learnAngular/DaMo/e2e/app.e2e-spec.ts
+++ _learnAngular/DaMo/e2e/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { DePage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('de App', () => {
-  let page: DePage;
+describe('da-mo App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new DePage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
