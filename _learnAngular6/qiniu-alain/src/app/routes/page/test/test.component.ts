527621943beb8fba08281bbbf9ff2074d7661c13
amayer <454593034@qq.com>
2018-07-31 21:33:02 +0800

    qiniu有问题

--- _learnAngular6/qiniu-alain/src/app/routes/page/test/test.component.ts
+++ _learnAngular6/qiniu-alain/src/app/routes/page/test/test.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
-import { Config as my } from "../../../core/config";
-import * as qiniu from "qiniu"
+import { Config  } from "../../../core/config";
+//import * as qiniu from "qiniu"
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
@@ -14,13 +14,13 @@ export class TestComponent implements OnInit {
   ) { }
 
   ngOnInit() {     
-      qiniu.conf.ACCESS_KEY=my.ak;
-      qiniu.conf.SECRET_KEY=my.sk;
+     // qiniu.conf.ACCESS_KEY=""//my.ak;
+      //qiniu.conf.SECRET_KEY=""//my.sk;
   }
   // qiniu = require("qiniu");
   //qiniu.conf.ACCESS_KEY = '<Your Access Key>'
   //qiniu.conf.SECRET_KEY = '<Your Secret Key>'   
-   ak = my.ak;
-   sk = my.sk;
+   ak = Config.ak;
+   sk = Config.sk;
    
 }
