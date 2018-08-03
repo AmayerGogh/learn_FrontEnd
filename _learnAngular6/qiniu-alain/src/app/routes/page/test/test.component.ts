22776bbf58d7ef7210c2fedcb0e7545ed6484f3c
amayer <454593034@qq.com>
2018-08-03 21:43:00 +0800

    新增包 qiniu4js

--- _learnAngular6/qiniu-alain/src/app/routes/page/test/test.component.ts
+++ _learnAngular6/qiniu-alain/src/app/routes/page/test/test.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
 import { Config  } from "../../../core/config";
-//import * as qiniu from "qiniu"
+//import * as qiniu from "qiniu-js"
+import {UploaderBuilder,Uploader} from 'qiniu4js'
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
@@ -11,15 +12,20 @@ export class TestComponent implements OnInit {
 
   constructor(
     private http: _HttpClient
-  ) { }
+  ) {
+    let uploader = new UploaderBuilder()
+   }
 
   ngOnInit() {     
+    
+  //   conf.ACCESS_KEY="1234";
      // qiniu.conf.ACCESS_KEY=""//my.ak;
       //qiniu.conf.SECRET_KEY=""//my.sk;
   }
   // qiniu = require("qiniu");
   //qiniu.conf.ACCESS_KEY = '<Your Access Key>'
   //qiniu.conf.SECRET_KEY = '<Your Secret Key>'   
+    //mac = new qiniu.auth.digest.Mac("", "");
    ak = Config.ak;
    sk = Config.sk;
    
