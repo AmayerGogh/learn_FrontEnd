817fcb444cf72596ccb08c3f4ba782a5396b543a
amayer <454593034@qq.com>
2018-08-10 09:23:31 +0800

    禁用样式封装

--- _learnAngular6/qiniu-alain/src/app/routes/page/test2/test2.component.ts
+++ _learnAngular6/qiniu-alain/src/app/routes/page/test2/test2.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, Input, Output, forwardRef, EventEmitter, ElementRef, Renderer, OnChanges, OnInit } from '@angular/core';
+import { Component, ViewChild, Input, Output, forwardRef, EventEmitter, ElementRef, Renderer, OnChanges, OnInit, ViewEncapsulation } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 import { _HttpClient } from '@delon/theme';
 import { Config  } from "../../../core/config";
@@ -7,7 +7,8 @@ import {CropperHelper} from './cropper'
 @Component({
   selector: 'app-test2',
   templateUrl: './test2.component.html',
-  styleUrls:['../../../../assets/common/css/bootstrap.css', './test2.component.css']
+  styleUrls:['./test2.component.less'], //'../../../../assets/common/css/bootstrap.css', 
+  encapsulation:ViewEncapsulation.None //禁用样式封装 限制自动生成[_ngcontent-c6]等
 })
 export class Test2Component implements OnInit  {
   jQuery:any;
