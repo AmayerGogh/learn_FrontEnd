790514c3cd957892b6a31057cd1a92b0116be093
AmayerGogh <454593034@qq.com>
2018-10-19 08:42:47 +0800

    .

--- project/weiji/src/code/round.ts
+++ project/weiji/src/code/round.ts
@@ -1,18 +1,42 @@
 class Round{
 
-  public  static A:Role     
+  currentStep:number;  
+  public  A:Role     
   public  B:Role
+  //前5回合动作
+  aboveTempA:Array<any>
+  aboveTempB:Array<any>
     Begin(){
         //Reflect.getPrototypeOf();
     }
     
     
     r(){
+        this.currentStep++;
+
         //this.A_Step[0];
         //this.A_Step[0];
         //同步
     }
 
     //skillArea list
-    //状态机
+    order:Array<string>;
+    doA_completed:boolean
+    dbB_completed:boolean
+    do(){
+        if(this.order[0]=="a" && !this.doA_completed){
+            //doa
+            this.doA_completed = true;
+        }
+        else{
+            this.do();
+        }
+        if(this.order[0]=="b" && !this.dbB_completed){
+            //doa
+            this.dbB_completed = true;
+        }
+        else{
+            this.do();
+        }
+    }
 }
