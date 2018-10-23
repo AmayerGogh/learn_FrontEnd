0eea2742bc6c200da61faa5d2dd2e1938be10419
amayer <454593034@qq.com>
2018-10-23 21:04:57 +0800

    .

--- project/weiji/src/code/skillClass/skillArea/skillArea.ts
+++ project/weiji/src/code/skillClass/skillArea/skillArea.ts
@@ -1,15 +1,32 @@
+import { JsonPipe } from "@angular/common";
+
 interface skillArea{
     creater:any;
-    hurt:number;
+    //hurt:number;
 
-    初始位置:[0,0]
-    角度:number;
+
+     /**
+     * 触发起始位置 
+     */
+    startPosition:[0,0,0]
+    oneOrMore:oneOrMore;
+
+     /**
+     * 范围
+     */
+    shape:Array<Shape>
+
+     /**
+     * 角度
+     */
+    angle:number;
     角度追踪:number;
 
-    范围:Array<AreaType>
-    范围负:Array<AreaType>
-
-    demageType:demageType;
+     /**
+     * 覆盖类型
+     */
+    areaType:AreaType
+    //demageType:demageType;
     
     //affect
     //releasing
@@ -17,19 +34,46 @@ interface skillArea{
 }
 
 interface AreaType{
-
+    全覆盖,
+    渐进,
+    子弹
+}
+interface Shape{
+    //距离起始位置
+    startP,
+    //距离起始位置角度
+    startL,
+    
 }
 
-class 矩形{
-    height:number;
-    width:number;
+
+class 矩形 implements Shape{
+    startP: any;
+    startL: any;
+   public  height:number;
+   public   width:number;
 }
-class 扇形{
-    半径:number;
-    角度:number;
+class 扇形 implements Shape{
+    startP: any;
+    startL: any;
+   public  半径:number;
+   public  角度:number;
 }
-enum demageType{
+
+enum oneOrMore{
     单一目标,
-    全部范围
-
+    范围内,
+    范围内限定
 }
+
+//案例 椭圆
+
+var j1= new 矩形()
+j1.height=10
+j1.width=3
+var s1= new 扇形()
+s1.半径=1.5
+s1.角度=360
+var s1= new 扇形()
+s1.半径=1.5
+s1.角度=360
